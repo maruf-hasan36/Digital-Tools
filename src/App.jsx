85386eeb721f8300c05d6f72bab819e858a73884
nexarphantom <marufhasanbr@gmail.com>
2026-04-01 11:49:32 +0600

    remove a tab bug

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const productsPromise = fetchProductsData();
 function App() {
   // const productsPromise = fetchProductsData();
 
-  const [activeTab, setActiveTab] = useState("product");
+  const [activeTab, setActiveTab] = useState("prod");
 
   const [carts, setCarts] = useState([]);
   // console.log(carts);
